c2fd6de7be9279cdd938f619c26fe7a46bfb69bd
Vignesh Sathyanarayanan <vicky41298@gmail.com>
2026-07-19 23:13:47 +0530

    Deployment bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Personal_bot/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
